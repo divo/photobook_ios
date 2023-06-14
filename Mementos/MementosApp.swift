66ef1b502c4de6ee4870efbee0803f41894fad6c
divo <divoapple@gmail.com>
2023-06-14 15:03:54 +0100

    Inital commit

---
+++ Mementos/MementosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MementosApp.swift
+//  Mementos
+//
+//  Created by Steven Diviney on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MementosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
