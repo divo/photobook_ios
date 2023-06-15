4c4972b3f202c5a5c7102c42e3b52c078c15b798
divo <divoapple@gmail.com>
2023-06-15 15:38:19 +0100

    Basic data model to retrieve cookie
    I'll probably have to redo all of this when I know what I want

--- Mementos/WebView.swift
+++ Mementos/WebView.swift
@@ -14,12 +14,16 @@ struct WebView: UIViewRepresentable {
   let webDataStore = WKWebsiteDataStore.default()
   let configuration: WKWebViewConfiguration
   let webView: WKWebView
+  let dataModel: WebViewDataModel
   
   init(url: URL) {
     self.url = url
     self.configuration = WKWebViewConfiguration()
     configuration.websiteDataStore = webDataStore
     self.webView = WKWebView(frame: .zero, configuration: configuration)
+    self.dataModel = WebViewDataModel()
+    
+    dataModel.webView = self
   }
   
   func makeUIView(context: Context) -> some UIView {
@@ -30,5 +34,16 @@ struct WebView: UIViewRepresentable {
   func updateUIView(_ uiView: UIViewType, context: Context) {
     let request = URLRequest(url: url)
     (uiView as? WKWebView)?.load(request)
+  }
+}
+
+class WebViewDataModel: ObservableObject {
+  @Published var railsCookie: HTTPCookie?
+  var webView: WebView! // Mist be a cleaner pattern for this, I've just forgotten it
+  
+  func fetch_rails_cookie() {
+    webView.webDataStore.httpCookieStore.getAllCookies { cookies in
+      self.railsCookie = cookies.first { cookie in cookie.name == "_photobook_rails_session" }
+    }
   }
 }
