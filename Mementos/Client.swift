87754f2448b6b4235003310c53677fd1bf3fed59
divo <divoapple@gmail.com>
2023-06-16 15:11:20 +0100

    Failed attempt at grabbing CSRF token and using in POST requests
    Rails CSRF is doing something smarter than me, and I've spent enough time on this.
    Taking the easy insecure option. Committing this in case I want to re-visit it (lol)

--- Mementos/Client.swift
+++ Mementos/Client.swift
@@ -24,15 +24,72 @@ class Client {
 //  }
   
   public func test() {
-    let url = "http://192.168.0.88:3000/photo_albums"
+    let url = "http://192.168.0.88:3000/photo_albums.json"
+    // The cookie should always be there as Rails will prompt the user to login if needed
+    // and we try to grab it after every request.
+    // If cookie is not there we shouldn't have got this far and crash
     let cookies = AF.session.configuration.httpCookieStorage!.cookies!
     let header_cookie = HTTPCookie.requestHeaderFields(with: cookies)
-    var headers = HTTPHeaders(header_cookie)
+    let headers = HTTPHeaders(header_cookie)
     
     AF.request(url, headers: headers).responseString { response in
       print(response.value)
     }
     
+  }
+  
+  func create_album(title: String) {
+    let url = "http://192.168.0.88:3000/photo_albums.json"
+    let params = ["title": title]
+    
+    // The cookie should always be there as Rails will prompt the user to login if needed
+    // and we try to grab it after every request.
+    // If cookie is not there we shouldn't have got this far and crash
+    let cookies = AF.session.configuration.httpCookieStorage!.cookies!
+    let header_cookie = HTTPCookie.requestHeaderFields(with: cookies)
+    var headers = HTTPHeaders(header_cookie)
+    
+    AF.request("http://192.168.0.88:3000/photo_albums/new", method: .get, headers: headers).responseString { response in
+      switch response.result {
+      case.success(let body):
+        if let headerFields = response.response?.allHeaderFields as? [String: String],
+           let URL = response.request?.url
+        {
+          let cookies = HTTPCookie.cookies(withResponseHeaderFields: headerFields, for: URL)
+          let csrfToken = self.extractCSRFToken(from: body)!
+          var fresh_headers = HTTPHeaders(HTTPCookie.requestHeaderFields(with: cookies))
+//          var fresh_headers = HTTPHeaders(headerFields)
+          fresh_headers.add(name: "authenticity_token", value: csrfToken)
+          AF.request(url, method: .post, parameters: params, headers: fresh_headers).responseString { response in
+            print(response)
+          }
+        }
+        
+     case .failure(let error):
+        // TODO: Something
+        print(error.localizedDescription)
+      }
+    }
+ }
+  
+  // Code grabbed from ChatGPT, a bit out of date but this is all hacky anyway
+  func extractCSRFToken(from string: String) -> String? {
+      let pattern = "<meta name=\"csrf-token\" content=\"([^\"]+)\" />"
+      
+      do {
+          let regex = try NSRegularExpression(pattern: pattern, options: [])
+          let nsString = string as NSString
+          let match = regex.firstMatch(in: string, options: [], range: NSRange(location: 0, length: nsString.length))
+          
+          if let matchRange = match?.range(at: 1) {
+              let csrfToken = nsString.substring(with: matchRange)
+              return csrfToken
+          }
+      } catch {
+          print("Error creating regex: \(error)")
+      }
+      
+      return nil
   }
   
   public func upload(_ data: [String: Data]) {
