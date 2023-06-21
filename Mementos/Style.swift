1eef7435cfd3d980d10cbafe2f256ce3893dc0ce
divo <divoapple@gmail.com>
2023-06-21 15:19:28 +0100

    Fix colors

--- Mementos/Style.swift
+++ Mementos/Style.swift
@@ -12,10 +12,10 @@ import UIKit
 struct Style {
   // TODO: Color values from reails app don't look right here, must be some other effects applied
   static func primaryColor() -> Color {
-    return Color(uiColor: UIColor(red: 90, green: 85, blue: 239, alpha: 1))
+    return Color(red: 90 / 255, green: 85 / 255, blue: 239 / 255)
   }
   
   static func secondaryColor() -> Color {
-    return Color(uiColor: UIColor(hue: 0.66, saturation: 0.94, brightness: 0.81, alpha: 1.0))
+    return Color(red: 61 / 255, green: 164 / 255, blue: 252 / 255)
   }
 }
