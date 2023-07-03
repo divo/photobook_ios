312bb7833c3d3cf6e363559ac4c18ca4e0231062
divo <divoapple@gmail.com>
2023-07-03 11:10:25 +0100

    Switch testing back to LAN address

--- Mementos/MementosApp.swift
+++ Mementos/MementosApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Constants {
 #if DEBUG
+//  static let baseURL = "http://localhost:3000"
   static let baseURL = "http://192.168.0.88:3000"
 #else
   static let baseURL = "https://mementos.ink"
