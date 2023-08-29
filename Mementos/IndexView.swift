0aac5b5debe4495a47928804d41f1502a317a25f
divo <divoapple@gmail.com>
2023-08-29 12:56:11 +0100

    Break out IndexViewModel into own class

--- Mementos/IndexView.swift
+++ Mementos/IndexView.swift
@@ -8,41 +8,6 @@
 import SwiftUI
 import Network
 
-class IndexViewModel: ObservableObject {
-  @Published var url = URL(string: Constants.baseURL + "/photo_albums")!
-  @Published var showUrl = URL(string: Constants.baseURL)!
-  @Published var profileUrl = URL(string: Constants.baseURL + "/users/edit/")!
-  // TODO: Consilidate bools into one string
-  @Published var pushNew: Bool = false
-  @Published var pushShow: Bool = false
-  @Published var pushProfile: Bool = false
-  @Published var childTitle: String = "Mementos"
-  @Published var profileTitle: String = "Profile"
-  
-  let monitor = NWPathMonitor()
-  let queue = DispatchQueue(label: "Monitor")
-  @Published private(set) var connected: Bool = true
-  
-  init() {
-    checkConnection()
-  }
-  
-  func checkConnection() {
-    monitor.pathUpdateHandler = { path in
-      if path.status == .satisfied {
-        DispatchQueue.main.async {
-          self.connected = true
-        }
-      } else {
-        DispatchQueue.main.async {
-          self.connected = false
-        }
-      }
-    }
-    monitor.start(queue: queue)
-  }
-}
-
 struct IndexView: View {
   @ObservedObject var viewModel = IndexViewModel()
 
---
+++ Mementos/IndexViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  IndexViewModel.swift
+//  Mementos
+//
+//  Created by Steven Diviney on 28/08/2023.
+//
+
+import SwiftUI
+import Network
+
+class IndexViewModel: ObservableObject {
+  @Published var url = URL(string: Constants.baseURL + "/photo_albums")! {
+    didSet {
+      // Set a breakpoint here, although it's more than likely
+      // ordinary UI updates causing the view to redraw
+    }
+  }
+  @Published var showUrl = URL(string: Constants.baseURL)!
+  @Published var profileUrl = URL(string: Constants.baseURL + "/users/edit/")!
+  // TODO: Consilidate bools into one string
+  @Published var pushNew: Bool = false
+  @Published var pushShow: Bool = false
+  @Published var pushProfile: Bool = false
+  @Published var childTitle: String = "Mementos"
+  @Published var profileTitle: String = "Profile"
+  
+  let monitor = NWPathMonitor()
+  let queue = DispatchQueue(label: "Monitor")
+  @Published private(set) var connected: Bool = true
+  
+  init() {
+    checkConnection()
+  }
+  
+  func checkConnection() {
+    monitor.pathUpdateHandler = { path in
+      if path.status == .satisfied {
+        DispatchQueue.main.async {
+          self.connected = true
+        }
+      } else {
+        DispatchQueue.main.async {
+          self.connected = false
+        }
+      }
+    }
+    monitor.start(queue: queue)
+  }
+}
